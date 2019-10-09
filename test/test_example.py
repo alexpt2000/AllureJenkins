55bd8e8c316b7435b2627765a8e32291fa5bf80f
alex <alexpt2000@gmail.com>
2019-10-09 13:29:13 +0100

    Update test_example.py

--- test/test_example.py
+++ test/test_example.py
@@ -5,4 +5,11 @@ def test_foo():
         assert(True == True)
 
     with nose.allure.step('step two'):
+        assert(False == True)
+
+def test_Othe():
+    with nose.allure.step('step one'):
+        assert(True == True)
+
+    with nose.allure.step('step two'):
         assert(False == False)
